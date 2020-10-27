46f3be57344ca57d42fd1398da58d5c36c7b8459
azn4lifee <anthonyma940603@gmail.com>
2020-10-26 20:51:01 -0400

    Added resource

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from webscraper.api import api, app
 from webscraper.public.routes import bp
-from webscraper.api.routes import ProductApi, ProfileApi
+from webscraper.api.routes import HistoryApi, ProductApi, ProfileApi
 
 if __name__ == "__main__":
 
     api.add_resource(ProductApi, "/api/products", "/api/products/<int:product_id>")
     api.add_resource(ProfileApi, "/api/profile")
+    api.add_resource(HistoryApi, "/api/history", "/api/history/<int:id>")
     app.register_blueprint(bp)
 
     app.run()
