e4682e1f24ac0a0dabdb52fcad9f9d307ee0992b
cmckail <mckail96@gmail.com>
2020-10-26 22:50:40 -0400

    Fixed issue with Referr code in url

--- webscraper/public/static/js/main.js
+++ webscraper/public/static/js/main.js
@@ -1,38 +1,39 @@
 const getProducts = {
-    async: true,
-    crossDomain: true,
-    url: "http://localhost:5000/api/products?=",
-    method: "GET",
-    data: {},
-    success: function (response) {
-        updateTable(response);
-    },
-    error: function (response) {
-        alert(`error updating from api\n ${response}`);
-    },
+  async: true,
+  crossDomain: true,
+  url: "http://localhost:5000/api/products?=",
+  method: "GET",
+  data: {},
+  success: function (response) {
+    updateTable(response);
+  },
+  error: function (response) {
+    alert(`error updating from api\n ${response}`);
+  },
 };
 
 const postProducts = {
-    url: "http://localhost:5000/api/products?=",
-    method: "POST",
-    data: { url: "" },
-    dataType: "json",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    success: function (response) {
-        $.ajax(getProducts);
-        console.log("success");
-    },
-    error: function (response) {
-        alert(`Error sending from api ${response}`);
-    },
+  url: "http://localhost:5000/api/products?=",
+  method: "POST",
+  data: { url: "" },
+  dataType: "json",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  success: function (response) {
+    $.ajax(getProducts);
+    console.log("success");
+  },
+  error: function (response) {
+    alert(`Error sending from api ${response}`);
+  },
 };
 
 function updateTable(response) {
-    console.log(response);
-    $.each(response, function (i, product) {
-        $("#tableBody").append(`
+  console.log(response);
+  $.each(response, function (i, product) {
+    $("#tableBody").empty();
+    $("#tableBody").append(`
         <tr>
             <td scope="row">
                 ${product["id"]}
@@ -51,21 +52,21 @@ function updateTable(response) {
             </td>
         </tr>
     `);
-    });
+  });
 }
 
 $("#addBtn").click(function (e) {
-    e.preventDefault();
-    url = $("#url").val();
-    if (url == "") {
-        alert("No value for URL");
-        return false;
-    } else {
-        request = postProducts;
-        request["data"] = `{"url": "${url}"}`;
-        console.log(request);
-        $.post(request);
-    }
+  e.preventDefault();
+  url = $("#url").val();
+  if (url == "") {
+    alert("No value for URL");
+    return false;
+  } else {
+    request = postProducts;
+    request["data"] = `{"url": "${url}"}`;
+    console.log(request);
+    $.post(request);
+  }
 });
 
 $.ajax(getProducts);
